caff14819a6f3d5f8d0a9a35f6c12b15a9ee6b21
Chris Leary <learymchris@gmail.com>
2023-07-31 21:25:13 -0400

    Completed Labb

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+let hq =42;
+
+function distanceFromHqInBlocks(distance) {
+ return Math.abs(hq-distance);
+}
+  
+  function distanceFromHqInFeet(distance) {
+    
+    return distanceFromHqInBlocks(distance) * 264;
+  }
+  
+  function distanceTravelledInFeet(block1, block2) {
+
+    return Math.abs(block1 - block2) * 264;
+  }
+
+  function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance < 400){
+      return 0;
+    }
+    else if (distance <= 2000){
+      return (distance-400) * .02;
+    }
+    else if (distance <= 2500){
+      return 25;
+    }
+    else if (distance > 2500){
+      return 'cannot travel that far'
+    }
+  }
